7f2c56ea8b9d10974d67d4809c784af170bb6e99
aucelio0 <mateusaucelio06@gmail.com>
2024-08-23 14:46:23 -0300

    versão 1.0

---
+++ tradutor_app.py
@@ -0,0 +1,21 @@
+# biblioteca
+from deep_translator import GoogleTranslator
+
+# gerar tradução
+tradutor = GoogleTranslator(source='auto', target='pt')
+
+while True:
+    try:
+        texto = input('Texto a ser traduzido: ')
+        texto_traduzido = tradutor.translate(texto)
+        
+        print(f'Tradução: "{texto_traduzido}".')
+        
+        repetir = input('Deseja traduzir outro texto?(s/n)').lower()
+        
+        if repetir == 's':
+            continue
+        else:
+            break
+    except:
+        print('Não foi possível traduzir.')
